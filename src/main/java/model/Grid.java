7a52be1eb1fac5ef9e982fa4bcd46ced2d6c2388
Marian Luca <marianluca404@gmail.com>
2024-11-02 22:01:23 +0100

    Implemented Grid Class

--- src/main/java/model/Grid.java
+++ src/main/java/model/Grid.java
@@ -4,10 +4,66 @@
  */
 package model;
 
+import java.util.Set;
+
 /**
- *
- * @author 20232049
+ * This class represents the grid/ graph of cells
+ * @author Marian Luca
  */
 public class Grid {
+    // Values chosen based on the fact that a cell would be 10x10 pixels 
+    // on a 850x530 pixels panel
+    private static final int ROWS = 85;
+    private static final int COLS = 53;
+    private static final int NOCELLS = ROWS*COLS;
+    
+    // 2D array representing the graph/ grid
+    private Cell[][] grid;
+    //private int[][] graph;
+    
+    
+    public Grid() {
+        //graph = new int[NOCELLS][NOCELLS];
+        grid = new Cell[ROWS][COLS];
+        initialiseGrid();
+    }
+    
+    private void initialiseGrid(){
+        for (int i =0; i< ROWS; i++) {
+            for (int j=0; j< COLS; j++) {
+                grid[i][j] = new Cell(i, j);
+                
+            }
+        }
+    }
+    
+    public int getRows() {
+        return this.ROWS;
+    }
+    
+    public int getCols() {
+        return this.COLS;
+    }
+    
+    public Cell getCell(int row, int col) {
+        return grid[row][col];
+    }
+    
+    public void resetWalls(Set<Cell> walls) {
+        for (Cell cell: walls) {
+            cell.setStatus(Cell.Status.Normal);
+        }
+    }
+    
+    public void resetPath(Set<Cell> path) {
+        for (Cell cell: path) {
+            cell.setWeight(-1);
+        }
+    }
+    
+    public void resetEverything(Set<Cell> path, Set<Cell> walls) {
+        resetWalls(walls);
+        resetPath(path);
+    }
     
 }
