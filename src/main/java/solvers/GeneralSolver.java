d0d69fe3d35e03f82397b0b2e83ed02ac74702d4
Marian Luca <marianluca404@gmail.com>
2024-11-07 23:07:00 +0100

    Implemented GeneralSolver

--- src/main/java/solvers/GeneralSolver.java
+++ src/main/java/solvers/GeneralSolver.java
@@ -1,13 +1,39 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package solvers;
 
+package solvers;
+import java.util.concurrent.ExecutionException;
+import javax.swing.SwingWorker;
+import model.Cell;
+import model.Grid;
 /**
- *
- * @author 20232049
+ * This is an abstract class for the different algorithms classes implemented
+ * @author Marian Luca/ phantomCat1
  */
-public class GeneralSolver {
+public abstract class GeneralSolver {
+    private final Cell start;
+    private final Cell end;
+    private final Grid grid;
     
+    public GeneralSolver(Cell start, Cell end, Grid grid) {
+        this.start = start;
+        this.end = end;
+        this.grid = grid;
+    }
+    public abstract boolean solve();
+    public boolean runSolver() {
+        SwingWorker<Boolean, Void> solver = new SwingWorker<Boolean, Void>(){
+            @Override
+            protected Boolean doInBackground() throws Exception {
+                return solve();
+            }
+        
+        };
+        try {
+            return solver.get();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+        return false;   
+    }
 }
